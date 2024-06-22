771c669eab99c254decbb9e0feb84f8c3885bc3d
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-06-22 19:26:00 +0200

    + Projects section basic layout

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function myFunction() {
-   var x = document.querySelector("#nav-menu-list");
-   if (x.style.display === "block") {
-      x.style.display = "none";
-   } else {
-      x.style.display = "block";
-   }
+var slider_img = document.querySelector("#ruander-imgs");
+var images = ["frontend.jpg", "backend.webp", "django.jpg"];
+var i = 0;
+
+function prev() {
+   if (i <= 0) i = images.length;
+   i--;
+   return setImg();
+}
+
+function next() {
+   if (i >= images.length - 1) i = -1;
+   i++;
+   return setImg();
+}
+
+function setImg() {
+   return slider_img.setAttribute("src", "img/" + images[i]);
 }
