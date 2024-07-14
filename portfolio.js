345a9d83ab7de39ad850a97f9952775c4135f5eb
fulopzsolt1999 <fulopzsolt1999@gmail.com>
2024-07-14 11:23:28 +0200

    + Prevent bot spams

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,10 @@
 // Add a hover event listener to the spans to show "Copy to Clipboard" on hover
 document.querySelectorAll(".social-links").forEach((link) => {
-   link.addEventListener("click", (event) => {
+   link.addEventListener("click", () => {
       var copyText = link.textContent;
+      if (link.id == "email") {
+         copyText = copyText.split("!")[1];
+      }
 
       navigator.clipboard
          .writeText(copyText)
